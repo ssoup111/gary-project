e6dc1e5f47392f095463cedb88bd3d096eeb1d22
William Campbell <williamcampbell@williams-mini.lan>
2026-05-19 21:50:37 -0500

    Rename middleware to proxy

---
+++ proxy.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
+
+  if (url.pathname.startsWith("/admin")) {
+    if (basicAuth) {
+      const authValue = basicAuth.split(" ")[1];
+      const [user, password] = atob(authValue).split(":");
+
+      if (
+        user === process.env.ADMIN_USERNAME &&
+        password === process.env.ADMIN_PASSWORD
+      ) {
+        return NextResponse.next();
+      }
+    }
+
+    return new NextResponse("Admin login required", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="JPIX Admin"',
+      },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
